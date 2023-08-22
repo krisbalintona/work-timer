62705faffc176c41912ce8933050ec138cf8ceef
Kristoffer Balintona <krisbalintona@gmail.com>
2023-08-22 14:51:01 -0500

    fix: org-work-timer-pause-or-continue edge cases
    When forcing a continue when already continued and when forcing a pause when already paused.

--- org-work-timer.el
+++ org-work-timer.el
@@ -318,22 +318,30 @@ that action."
                               (if org-work-timer-pause-time
                                   'continue
                                 'pause)))
-  (pcase pause-or-continue
-    ('continue
-     ;; Move back `org-work-timer-end-time' for how long timer was paused
-     (setq org-work-timer-end-time (float-time
-                                    (time-add org-work-timer-end-time
-                                              (time-since org-work-timer-pause-time))))
+  (let (pauses-modified-p)
+    (pcase pause-or-continue
+      ('continue
+       (when org-work-timer-pause-time    ; Do nothing if not currently paused
+         ;; Move back `org-work-timer-end-time' for how long timer was paused
+         (setq org-work-timer-end-time (float-time
+                                        (time-add org-work-timer-end-time
+                                                  (time-since org-work-timer-pause-time))))
 
-     ;; Add pause period to `org-work-timer-pauses'
-     (setf (plist-get (car (last org-work-timer-pauses)) :pause-end)
-           (float-time (current-time)))
-     (setq org-work-timer-pause-time nil))
-    ('pause
-     (setq org-work-timer-pause-time (float-time (current-time)))
-     (setq org-work-timer-pauses
-           (append org-work-timer-pauses
-                   (list (list :pause-start org-work-timer-pause-time :pause-end nil)))))))
+         ;; Set :pause-end of pause in `org-work-timer-pauses'
+         (setf (plist-get (car (last org-work-timer-pauses)) :pause-end)
+               (float-time (current-time)))
+         (setq org-work-timer-pause-time nil
+               pauses-modified-p t)))
+      ('pause
+       (unless org-work-timer-pause-time  ; Do nothing if already paused
+         (setq org-work-timer-pause-time (float-time (current-time))
+               org-work-timer-pauses
+               (append org-work-timer-pauses
+                       (list (list :pause-start org-work-timer-pause-time :pause-end nil)))
+               pauses-modified-p t))))
+    ;; If actually paused or continued, it is useful to return
+    ;; `org-work-timer-pauses'
+    (when pauses-modified-p org-work-timer-pauses)))
 
 ;;;###autoload
 (defun org-work-timer-cycle-finish ()
